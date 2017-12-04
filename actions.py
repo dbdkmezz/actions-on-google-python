a4e3a60767cd3f361e5f2c3358a4d18cfe3f5c43
Paul Carver <dbdkmezz@gmail.com>
2017-12-04 08:42:36 +0000

    added conversation token support

--- actions.py
+++ actions.py
@@ -1,3 +1,5 @@
+# https://developers.google.com/actions/reference/rest/Shared.Types/AppResponse
+
 import logging
 
 
@@ -6,11 +8,18 @@ class GoogleActions(object):
         self.logger = logger
         self.sandbox = sandbox
 
-    def ask(self, text):
+    def ask(self, text, conversation_token=None):
+        """Continues the conversation.
+
+        text: the message to send back to the user
+        conversation_token: a token which will be recycled by Google back to us in the next request.
+           Can be used by an app to track the conversation or to store conversation related data.
+        """
+
         self.logger.debug('Asking {}'.format(text))
-        return {
+        response = self._base_response(self.sandbox, conversation_token)
+        response.update({
             'expectUserResponse': True,
-            'isInSandbox': self.sandbox,
             'expectedInputs': {
                 'inputPrompt': {
                     'richInitialPrompt': {
@@ -23,12 +32,20 @@ class GoogleActions(object):
                     }
                 ],
             }
-        }
+        })
+        return response
 
-    def tell(self, text):
-        """Ends the conversation"""
+    def tell(self, text, conversation_token=None):
+        """Ends the conversation
+
+        text: the message to send back to the user
+        conversation_token: a token which will be recycled by Google back to us in the next request.
+           Can be used by an app to track the conversation or to store conversation related data.
+        """
+
         self.logger.debug('Telling {}'.format(text))
-        return {
+        response = self._base_response(self.sandbox, conversation_token)
+        response.update({
             'expectUserResponse': False,
             'isInSandbox': self.sandbox,
             'finalResponse': {
@@ -36,7 +53,15 @@ class GoogleActions(object):
                     'items': [self._generate_response_item(text)],
                 }
             },
-        }
+        })
+        return response
+
+    @staticmethod
+    def _base_response(sandbox, conversation_token):
+        response = {'isInSandbox': sandbox}
+        if conversation_token:
+            response['conversationToken'] = conversation_token
+        return response
 
     @staticmethod
     def _generate_response_item(text):
--- request.py
+++ request.py
@@ -1,3 +1,5 @@
+# https://developers.google.com/actions/reference/rest/Shared.Types/AppRequest
+
 import json
 import pprint
 import logging
@@ -14,21 +16,26 @@ class GoogleRequest(object):
     def _parse_request(self, request):
         self.text = None
         self.user_id = None
+        self.conversation_id = None
+        self.conversation_token = None
 
         body = request.body.decode('utf-8')
-        self.logger.info('Request body: %s', pprint.pformat(body))
         try:
             j = json.loads(body)
         except json.JSONDecodeError:
+            self.logger.info('Request body: %s', body)
             self.logger.warn('Unable to decode json')
             raise NoJsonException
+        self.logger.info('Request body: %s', pprint.pformat(j))
+
+        self.conversation_id = j['conversation']['conversationId']
+        self.conversation_token = j['conversation'].get('conversationToken')
 
         self.user_id = j['user']['userId']
         self.logger.info('USER ID: %s', self.user_id)
-        input = j['inputs'][0]
 
+        input = j['inputs'][0]
         if 'arguments' not in input:
             self.logger.info('No arguments in request')
             return None
-
         self.text = input['arguments'][0]['textValue']
