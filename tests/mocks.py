26713f0f73a0c1e3964382cd521313f826f6693b
Paul Carver <dbdkmezz@gmail.com>
2017-12-18 07:45:06 +0000

    made it possible to set the user id and conversation id in a mock request

--- tests/mocks.py
+++ tests/mocks.py
@@ -2,23 +2,27 @@ import json
 
 
 class MockRequest(object):
-    def __init__(self, body=None, argument_text_value=None):
-        if body and argument_text_value:
-            raise ValueError
+    def __init__(self, body=None, text=None, user_id=None, conversation_id=None):
+        if body and (text or user_id or conversation_id):
+            raise ValueError('If body is set you may not set any other paramaters.')
         self._body = body
-        self.argument_text_value = argument_text_value
+
+        if not self._body:
+            if text is None:
+                text = 'TEXT_VALUE'
+            if user_id is None:
+                user_id = 'USER_ID'
+            if conversation_id is None:
+                conversation_id = 'CONVERSATION_ID'
+
+            self._body = json.dumps({
+                'user': {'userId': user_id},
+                'conversation': {'conversationId': conversation_id},
+                'inputs': [{'arguments': [{'textValue': text}]}],
+            })
 
     @property
     def body(self):
-        if self._body:
-            return self._body.encode('utf-8')
-
-        if not self.argument_text_value:
-            input_dict = {}
-        else:
-            input_dict = {'arguments': [{'textValue': self.argument_text_value}]}
-        return json.dumps({
-            'user': {'userId': 'TEST_USER_ID'},
-            'conversation': {'conversationId': 'TEST_CONVERSATION_ID'},
-            'inputs': [input_dict],
-        }).encode('utf-8')
+        # Encode as utf-8 here, in case _body has intentially been set to a none utf-8 value to
+        # throw an exception.
+        return self._body.encode('utf-8')
