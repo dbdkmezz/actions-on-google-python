885f2b1aa83307c50107df1cdf3e61a2c7f7af44
Paul Carver <dbdkmezz@gmail.com>
2018-04-29 12:51:56 +0100

    added health check to mock request

--- tests/mocks.py
+++ tests/mocks.py
@@ -3,7 +3,7 @@ import json
 
 class MockRequest(object):
     def __init__(self, body=None, text=None, user_id=None, conversation_token=None,
-                 conversation_id=None):
+                 conversation_id=None, health_check=False):
         if body and (text or user_id or conversation_token or conversation_id):
             raise ValueError('If body is set you may not set any other paramaters.')
         self._body = body
@@ -16,15 +16,19 @@ class MockRequest(object):
             if conversation_id is None:
                 conversation_id = 'CONVERSATION_ID'
 
+            arguments = []
+            if text:
+                arguments.append({'name': 'text', 'textValue': text, 'rawText': text})
+            if health_check:
+                arguments.append({'name': 'is_health_check', 'textValue': '1', 'boolValue': True})
+
             self._body = json.dumps({
                 'user': {'userId': user_id},
                 'conversation': {
                     'conversationId': conversation_id,
                     'conversationToken': conversation_token,
                 },
-                'inputs': [{'arguments': [
-                    {'name': 'text', 'textValue': text, 'rawText': text}
-                ]}],
+                'inputs': [{'arguments': arguments}],
             })
 
     @property
